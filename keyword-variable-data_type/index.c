5cadffd976903f6d3eb1db88bdc945735b482800
siamparvez001 <siamparvez003@gmail.com>
2026-08-21 23:12:59 +0600

    add keyword variable data

---
+++ keyword-variable-data_type/index.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+int main()
+{
+    // variable diclaration
+    // type 1
+    int num1;
+    int num2;
+    float num3;
+    double num4;
+    char ch;
+    num1 = 10;
+    num2 = 20;
+    num3 = 2.4;
+    num4 = 4.35464353;
+    ch = "Siam";
+
+    //  type2
+    int num5, num6;
+    float num7;
+    double num8;
+    char ch;
+
+    num5 = 30;
+    num6 = 50;
+    num7 = 2.4;
+    num8 = 4.35464353;
+    ch = "Siam";
+
+    // type 3
+    int num9 = 70, num10 = 50;
+    float num11 = 3.3;
+    double num12 = 3.45763;
+    char ch = "Siam";
+
+    // Rouls for Naming Variables
+    /*
+        1.valid variable name == a...z, A....Z, 0....9, _, $
+        2.variable's 1st letter can't be a number(0...9)
+        3.can't be use a keyword as a variable name
+        4.there should be no space in a variable name
+        5. A variable name can contain a maximum of 31 characters, but it is better to use up to 8 characters
+    */
+    
+}
