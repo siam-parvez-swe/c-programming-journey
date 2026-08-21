6686960e7c8485c7487e9225c40c9173ffbb1878
siamparvez001 <siamparvez003@gmail.com>
2026-08-21 21:00:53 +0600

    explore Escape Sequence

---  01-first c programme/firxt.c
+++  01-first c programme/firxt.c
@@ -1,6 +1,14 @@
-#include<stdio.h>
+// write a programme that print your name
+#include <stdio.h>
 int main()
 {
-    printf("This is Siam Parvez");
+    printf("This is Siam Parvez");               // printing my name
+    printf("Daffodil International University"); // printing my university name
     return 0;
+    // single line comment
+    /*multiple line comment
+        siam parvez
+        sena public school and collage
+        Daffodil International University
+    */
 }
---
+++ escape sequence/escape.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int main()
+{
+    printf("Escape Sequence");
+    printf("Escape Sequence\a");
+    printf("Escape Sequence\b");
+    printf("Escape Sequence\f");
+    printf("Escape Sequence\n");
+    printf("Escape Sequence\r");
+    printf("Escape Sequence\t");
+    printf("Escape Sequence\v");
+    printf("Escape Sequence\0");
+    printf("Escape Sequence\'");
+    printf("Escape Sequence\"");
+    printf("Escape Sequence\\");
+    printf("Escape Sequence\?");
+    return 0;
+    /*
+        \a === (Alert) Bell
+        \b === BackSpace
+        \f === Form Feed
+        \n === NewLine
+        \r === Carrige Return
+        \t === Horizontal Tab
+        \v=== Vertical Tab
+        \0 === Null Character
+        \' === Single Quote
+        \"" === Double Quote
+        \\ === Backslash
+        \? === Questionmark
+    */
+}
