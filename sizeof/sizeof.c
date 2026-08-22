7367c90d471f5e7d5decf040eaf0151393dacec0
siamparvez001 <siamparvez003@gmail.com>
2026-08-22 18:53:16 +0600

    explore sizeof

---
+++ sizeof/sizeof.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{
+    int i;
+    float f;
+    double d;
+    char c;
+
+    printf("Size of int = %d bytes\n", sizeof(i));
+    printf("Size of float = %d bytes\n", sizeof(f));
+    printf("Size of double = %d bytes\n", sizeof(d));
+    printf("Size of char = %d bytes \n", sizeof(c));
+
+    return 0;
+}
