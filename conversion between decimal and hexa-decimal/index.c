e602c0c5003dcc92db8f9ff268660504a98e3df5
siamparvez001 <siamparvez003@gmail.com>
2026-08-23 05:22:33 +0600

    conversion between decimal and hexa-decimal

---
+++ conversion between decimal and hexa-decimal/index.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main()
+{
+    // decimal to hexa decimal
+    // int number;
+    // printf("Enter a decimal number : ");
+    // scanf("%d", &number);
+
+    // printf("Hexa decimal number = %x \n", number);
+
+
+    // hexa decimal to decimal
+    int number;
+    printf("Enter a hexa decimal number : ");
+    scanf("%x", &number);
+
+    printf("Hexa decimal = %d \n", number);
+
+    return 0;
+}
