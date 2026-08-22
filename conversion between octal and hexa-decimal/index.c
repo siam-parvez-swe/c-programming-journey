bcbe232347b62bccd93d98339eda6ccf915752cd
siamparvez001 <siamparvez003@gmail.com>
2026-08-23 05:29:09 +0600

    conversion between octal and hexa-decimal

---
+++ conversion between octal and hexa-decimal/index.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+int main()
+
+{
+    // octal to hexa decimal
+
+    // int number;
+    // printf("Enter a octal number : ");
+    // scanf("%o", &number);
+
+    // printf("Hexa decimal = %x \n", number);
+
+
+
+    // hexa decimal to octal
+
+    int num;
+    printf("Enter a hexa decimal number : ");
+    scanf("%x", &num);
+
+    printf("Octal number = %o \n", num);
+
+    return 0;
+}
