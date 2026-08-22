5a463dc97b7d1535ff2bca6552833d0bfaebf166
siamparvez001 <siamparvez003@gmail.com>
2026-08-23 00:51:45 +0600

    lower to uppercase letter using library function

--- toupper function/index.c
+++ toupper function/index.c
@@ -1,7 +1,11 @@
-#include<stdio.h>
-
+#include <stdio.h>
+#include <ctype.h>
 int main()
 {
-    
+    char lower, upper;
+    printf("Enter any lowercase letter : ");
+    scanf("%c", &lower);
+    upper = toupper(lower);
+    printf("Uppercase letter = %c \n", upper);
     return 0;
 }
