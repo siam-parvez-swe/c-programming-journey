c1fabe7df2d380ef1293a9cc956aade5de983ac1
siamparvez001 <siamparvez003@gmail.com>
2026-08-23 23:16:18 +0600

    explore Operator, Operand, Expression

---
+++ Operator, Operand, Expression/index.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    printf(" a + b = 7");
+
+    // a and b is operand
+    // + and = is operation
+    //  7 is constant
+    // a + b = 7 is expression
+    return 0;
+}
