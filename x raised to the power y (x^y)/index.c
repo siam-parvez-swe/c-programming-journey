8aefa041bba820b7cc857e25f5260c6c0d9c84cd
siamparvez001 <siamparvez003@gmail.com>
2026-08-25 02:18:57 +0600

    x raised to the power y (x^y)

---
+++ x raised to the power y (x^y)/index.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<math.h>
+
+int main()
+{
+    int x, y;
+
+    printf("Enter x = ");
+    scanf("%d", &x);
+
+    printf("Enter y = ");
+    scanf("%d", &y);
+
+    double result = pow(5,2);
+    printf("%.2lf \n", result);
+    
+    return 0;
+}
