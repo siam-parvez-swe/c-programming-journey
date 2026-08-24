56f7d1a2ffe238d022005252bbee84a7ecdadf46
siamparvez001 <siamparvez003@gmail.com>
2026-08-24 19:22:16 +0600

    Centigrade to Fahrenheit C program

---
+++ Centigrade to Fahrenheit/index.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main()
+{
+    float c,F;
+    printf("Enter centigrade : ");
+    scanf("%f", &c);
+    F = (c * 1.8) + 32;
+    printf("Fahrenheit = %.2f \n", F);
+    return 0;
+}
