8153923a1a2fa382ad4f3f6051573161a2e96bad
siamparvez001 <siamparvez003@gmail.com>
2026-08-25 01:22:30 +0600

    Fix quadratic equation calculation

--- Quadratic equation/index.c
+++ Quadratic equation/index.c
@@ -7,8 +7,10 @@ int main()
     printf("Enter a, b, c : ");
     scanf("%lf %lf %lf", &a, &b, &c);
     d = sqrt((b * b) - (4 * a * c));
+    
     x1 = (-b + d) / (2 * a);
     x2 = (-b - d) / (2 * a);
+
     printf("x1 = %lf \n", x1);
     printf("x2 = %lf \n", x2);
     return 0;
