9d3f0db8ae41533337e0c4cdecd12b8e9bf23566
siamparvez001 <siamparvez003@gmail.com>
2026-08-25 02:12:15 +0600

    Square root

--- Square root/index.c
+++ Square root/index.c
@@ -1,6 +1,12 @@
 #include<stdio.h>
+#include<math.h>
 
 int main()
 {
+    int x;
+    printf("Enter a number : ");
+    scanf("%d", &x);
+    double result = sqrt(x);
+    printf("%.2lf \n", result);
     return 0;
 }
