f4ee23c6a998a3d0629846fc3c73304a545cfc45
siamparvez001 <siamparvez003@gmail.com>
2026-08-24 19:34:54 +0600

    How to swap two numbers without temporary variable

---
+++ How to swap two numbers with temporary variable/index.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int num1 = 10;
+    int num2 = 5;
+    int temp;
+    
+    temp = num1;
+    num1 = num2;
+    num2 = temp;
+
+    printf("Num1 = %d \n", num1);
+    printf("Num2 =  %d \n", num2);
+
+    return 0;
+}
