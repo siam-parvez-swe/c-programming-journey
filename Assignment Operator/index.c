c6f75ceffe14e2ac5e930ff895e9dc3200753870
siamparvez001 <siamparvez003@gmail.com>
2026-08-25 02:42:34 +0600

    Assignment Operator

---
+++ Assignment Operator/index.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main()
+{
+    int a = 5;
+
+    a += 2;
+    printf("%d \n", a);
+    
+    a -= 2;
+    printf("%d \n", a);
+
+    a *= 2;
+    printf("%d \n", a);
+
+    a /= 2;
+    printf("%d \n", a);
+
+    return 0;
+}
