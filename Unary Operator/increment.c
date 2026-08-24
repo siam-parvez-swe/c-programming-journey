32125a1fd83b0069a6760ab657e27514e1a9bdb2
siamparvez001 <siamparvez003@gmail.com>
2026-08-25 02:58:29 +0600

    Increment

---
+++ Unary Operator/increment.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main()
+{
+    int x = 10;
+    int y = x++;
+    printf(" x = %d \n", x);
+    printf(" y = %d \n", y);
+    
+
+    int a = 5;
+    int b = ++a;
+    printf(" a = %d \n", a);
+    printf(" b = %d \n", b);
+
+    return 0;
+}
---
+++ Unary Operator/index.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main ()
+{
+    // Unary Operator       Meaning
+
+    // +                    Unary plus
+    // -                    Nuary minus
+    // ++                   Increment
+    // --                   Decrement
+
+    int x = 10;
+    int result1 = +x;
+    int result2 = -x;
+
+    printf("%d \n", result1);
+    printf("%d \n", result2);
+
+
+    
+
+    return 0;
+}
