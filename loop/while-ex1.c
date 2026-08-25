29c430204996776d3f3a2c188f9b3a8106908bb9
siamparvez001 <siamparvez003@gmail.com>
2026-08-26 01:57:34 +0600

    while loop example 1

--- loop/while-ex1.c
+++ loop/while-ex1.c
@@ -1,6 +1,14 @@
-#include<stdio.h>
+#include <stdio.h>
 
 int main()
 {
+    int n = 5;
+    int i = 1;
+    while (i <= 10)
+    {
+        printf("%d * %d = %d \n", n, i, n * i);
+        i++;
+    }
+
     return 0;
 }
