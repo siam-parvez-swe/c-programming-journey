0fd5835ebb57500070142d9a8bcb2015b759b451
siamparvez001 <siamparvez003@gmail.com>
2026-08-26 02:16:52 +0600

    for loop example no. 01

---
+++ loop/for-ex1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{
+    int m , n = 5, i;
+    m = 0;
+
+    for (i = 1 ;  i <= 10; i = i + 1)
+    {
+        m = m + n;
+        printf("%d * %d = %d\n", n, i, m);
+    }
+    
+    
+    return 0;
+}
