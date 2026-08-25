c9f3264c452d6809fc7b2c23719bdfcef62ef95f
siamparvez001 <siamparvez003@gmail.com>
2026-08-25 15:03:24 +0600

    Relational operators 2

---
+++ Relational operators 2/index.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int time = 18;
+    if (time == 10)
+    {
+        printf("Good Morning ");
+        printf("Siam Parvez \n");
+    }
+    else
+    {
+        printf("Sry, it's not morning \n");
+        printf("Siam Parvez \n");
+    }
+    return 0;
+}
---
+++ Relational operators 2/large.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+int main()
+{
+    int num1, num2;
+
+    printf("Enter first number : ");
+    scanf("%d", &num1);
+
+    printf("Enter second number : ");
+    scanf("%d", &num2);
+
+    if(num1>num2){
+        printf("Large = %d \n", num1);
+    }
+    else if(num1<num2){
+        printf("Large = %d \n", num2);
+    }
+    else{
+        printf("Numbers are equal . \n");
+    }
+
+    return 0;
+}
