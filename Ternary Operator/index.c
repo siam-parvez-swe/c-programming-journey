dcc857c3f3a8fb129ba2089cd44d180fa88b8e96
siamparvez001 <siamparvez003@gmail.com>
2026-08-27 01:01:55 +0600

    Ternary Operator

---
+++ Ternary Operator/index.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main()
+{
+    int num1, num2, large;
+    printf("Enter two number : ");
+    scanf("%d %d", &num1, &num2);
+
+    large = (num1>num2) ? num1 : num2;
+    printf("Large number = %d \n", large);
+
+    return 0;
+}
