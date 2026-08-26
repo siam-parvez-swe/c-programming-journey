898b28213616b5db0c251f4bbebc9d3b4ae0421e
siamparvez001 <siamparvez003@gmail.com>
2026-08-26 06:13:31 +0600

    for loop example 3

---
+++ loop/nested-loop.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, i;
+    printf("Enter a number : ");
+    scanf("%d", &n);
+    for (i = 10; i <= n; i++)
+    {
+        if (i % 11 == 0)
+        {
+            printf("%d \n", i);
+        }
+    }
+
+    return 0;
+}
