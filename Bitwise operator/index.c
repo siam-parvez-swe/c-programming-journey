bf44aa8b809dcdcae0ae41ee10bb59d2b302d97c
siamparvez001 <siamparvez003@gmail.com>
2026-08-27 01:07:24 +0600

    Bitwise operator simble and meaning

---
+++ Bitwise operator/index.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{
+    // Bitwise operator         Meaning
+
+    // &                        Bitwise AND
+    // |                        Bitwise OR
+    // ^                        Bitwise ExOR
+    // >>                       Right Shift
+    // <<                       Left Shift
+    // ~                        Bitwise NOT
+    
+
+    return 0;
+}
