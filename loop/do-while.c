a94a9adc4f98d8e8aaa82262e2c4ad9a5aac452f
siamparvez001 <siamparvez003@gmail.com>
2026-08-27 05:18:49 +0600

    do while loop

---
+++ loop/do-while.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main()
+{
+    int i = 1; // initialization
+    do
+    {
+        printf("%d. This is Siam Parvez \n", i);
+        i++; // increment or decrement
+    } while (i<=10); // condition
+    
+    return 0;
+}
