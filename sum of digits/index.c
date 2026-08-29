e394a59e5527522e95947b63f13028fef3588aaf
siamparvez001 <siamparvez003@gmail.com>
2026-08-30 05:29:14 +0600

    sum of digits

---
+++ sum of digits/index.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main()
+{
+    int num, temp, rem, sum = 0;
+
+    printf("Enter any number : ");
+    scanf("%d", &num);
+
+    temp = num;
+
+    while (temp != 0)
+    {
+        rem = temp % 10;
+        sum = sum + rem;
+        temp = temp / 10;
+    }
+
+    printf("Sum pf digit %d \n", sum);
+    
+
+    return 0;
+}
