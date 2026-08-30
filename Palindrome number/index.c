d9d02638d1914010545112ae15bb87f784bb5720
siamparvez001 <siamparvez003@gmail.com>
2026-08-30 06:34:07 +0600

    Palindrome number

---
+++ Palindrome number/index.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main()
+{
+    int num, temp, r, sum = 0;
+
+    printf("Enter any number : ");
+    scanf("%d", &num);
+
+    temp = num;
+
+    while (temp != 0)
+    {
+        r = temp % 10;
+        sum = sum * 10 + r;
+        temp = temp / 10;
+    }
+
+    if (num == sum)
+
+        printf("Palindrome number \n");
+
+    else
+
+        printf("Not Palindrome number \n");
+
+    return 0;
+}
