994373b381e23a826e7155307d83370b53b19afb
siamparvez001 <siamparvez003@gmail.com>
2026-08-31 23:37:14 +0600

    Calculate factorial of n

---
+++ factorial-of-n/index.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main()
+{
+    int i, n, result = 1;
+
+    printf("Enter n : ");
+    scanf("%d", &n);
+
+    for ( i = 1; i <= n; i++)
+    {
+        result = result * i;
+    }
+
+    printf("%d \n", result);
+    
+
+    return 0;
+}
