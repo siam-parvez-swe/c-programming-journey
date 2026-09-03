61150847a37eed36dc15208a2b2808c6796a349b
siamparvez001 <siamparvez003@gmail.com>
2026-09-04 01:37:43 +0600

    Hollow Triangle shape

---
+++ Pattern Type 5/Hollow Triangle shape/index.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, row, col;
+
+    printf("Enter the number of rows: ");
+    scanf("%d", &n);
+
+    for (row = 1; row <= n; row++)
+    {
+        for (col = 1; col <= n; col++)
+        {
+            if (row == col || row == n || col == 1 )
+            {
+                printf("* ");
+            }
+            else
+            {
+                printf("  ");
+            }
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
