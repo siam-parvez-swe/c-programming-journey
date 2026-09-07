2f0086d53fb3c00bb4a0f0cc634ac0da5c43a604
siamparvez001 <siamparvez003@gmail.com>
2026-09-07 06:31:15 +0600

    Maximum and Minimum of Array

--- Array/Maximum and Minimum of Array/index.c
+++ Array/Maximum and Minimum of Array/index.c
@@ -1,7 +1,33 @@
-#include<stdio.h>
+#include <stdio.h>
 
 int main()
 {
-    int num[100];
+    int num[100], n;
+
+    printf("Enter the number of elements in the array: ");
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &num[i]);
+    }
+
+    int max = num[0];
+    int min = num[0];
+
+    for (int i = 1; i < n; i++)
+    {
+        if (max < num[i])
+        {
+            max = num[i];
+        }
+        if (min > num[i])
+        {
+            min = num[i];
+        }
+    }
+    printf("Maximum number is: %d\n", max);
+    printf("Minimum number is: %d\n", min);
+
     return 0;
 }
