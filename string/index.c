0ad540c1577dcc0acc8662e5f8d481101b122b45
siamparvez001 <siamparvez003@gmail.com>
2026-09-08 12:00:42 +0600

    indroduction to string

---
+++ string/index.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    char s1[] = "Siam Parvez";
+
+    // s1[0]='S';
+    // s1[1]='i';
+    // s1[2]='a';
+    // s1[3]='m';
+
+    printf("S1 = %s \n", s1);
+
+    return 0;
+}
