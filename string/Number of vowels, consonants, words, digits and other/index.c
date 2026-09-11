0d8e5977a42b52d5fd84e6f5afd2ac2e411f90c7
siamparvez001 <siamparvez003@gmail.com>
2026-09-11 19:22:41 +0600

    Number of vowels, consonants, words, digits and other

---
+++ string/Number of vowels, consonants, words, digits and other/index.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+int main()
+{
+    char str[100], ch;
+    int i, vowel, consonant, digit, word, other;
+
+    printf("Enter a string ");
+    gets(str);
+
+    i = vowel = consonant = digit = word = other = 0;
+
+    while ((ch = str[i]) != '\0')
+    {
+        if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U')
+        {
+            vowel++;
+        }
+        else if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z'))
+        {
+            consonant++;
+        }
+        else if (ch >= '0' && ch <= '9')
+        {
+            digit++;
+        }
+        else if (ch == ' ')
+        {
+            word++;
+        }
+        else
+        {
+            other++;
+        }
+
+        i++;
+    }
+
+    word++;
+
+    printf("Number of vowel = %d \n", vowel);
+    printf("Number of consonant = %d \n", consonant);
+    printf("Number of vodigitwel = %d \n", digit);
+    printf("Number of word = %d \n", word);
+    printf("Number of other = %d \n", other);
+
+    return 0;
+}
