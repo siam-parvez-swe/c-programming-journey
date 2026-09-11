1103c25f0858ae3ac00c06d29552c5d4db457793
siamparvez001 <siamparvez003@gmail.com>
2026-09-11 23:37:12 +0600

    Sum of 2 numbers

---
+++ function/Sum of 2/index.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int sum(int a, int b)
+{
+    return a + b;
+}
+
+int main()
+{
+    int num1, num2;
+
+    printf("Enter two number : ");
+    scanf("%d %d", &num1, &num2);
+
+    int result = sum(num1, num2);
+
+    printf("The sum is %d \n", result);
+    return 0;
+}
