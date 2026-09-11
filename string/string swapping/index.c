7d8b2f0aea9a27184470a517b102eb2f416b68aa
siamparvez001 <siamparvez003@gmail.com>
2026-09-11 17:58:13 +0600

    string swapping

---
+++ string/string swapping/index.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char str1[15] = "Bangladesh";
+    char str2[15] = "Finland";
+    char temp[15];
+
+    strcpy(temp, str1);
+    strcpy(str1, str2);
+    strcpy(str2, temp);
+
+    printf("Str1 = %s \n", str1);
+    printf("Str2 = %s \n", str2);
+
+    return 0;
+}
